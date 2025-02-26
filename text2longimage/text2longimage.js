e486ca5eaccc20fbca71c3f5882b32f0b2929944
Charry Wu <wuzc.charry2013@gmail.com>
2025-02-25 19:47:01 -0800

    feat: update page to include links & support dark mode

--- text2longimage/text2longimage.js
+++ text2longimage/text2longimage.js
@@ -6,7 +6,7 @@ const DEFAULT_IMG_CONFIG = {
   padding: 42,
 };
 
-const $ = (id) => document.getElementById(id);
+const $$ = (id) => document.getElementById(id);
 function justifyText(text, maxCharsAllowedPerLine) {
   let curLineCharCount = 0;
   return text.replace(/[\S\s]/g, (char) => {
@@ -23,36 +23,38 @@ function justifyText(text, maxCharsAllowedPerLine) {
   });
 }
 
-function textToImg(textAreaId, canvasId, config = DEFAULT_IMG_CONFIG) {
-  let userText = $(textAreaId).value;
-  let $canvas = $(canvasId);
+function textToImg(textAreaId, canvasId, darkMode, config = DEFAULT_IMG_CONFI) {
+  let userText = $$(textAreaId).value;
+  let $canvas = $$(canvasId);
   if (!$canvas) return;
   if (userText == "") {
     alert("请输入文字！");
     $textArea.focus();
   }
   const { charsPerLine, fontSize, fontWeight, padding, lineSpacing } = config;
+  const bgColor = darkMode ? "#222" : "#fff";
+  const fgColor = darkMode ? "#fff" : "#222";
   const txtWithLineBreaks = justifyText(userText, charsPerLine * 2);
   let textLines = txtWithLineBreaks.split("\n");
   $canvas.width = fontSize * charsPerLine + padding * 2;
   $canvas.height = fontSize * lineSpacing * textLines.length + padding * 2;
-  $canvas.style.background = "white";
+  $canvas.style.background = bgColor;
   let canvasContext = $canvas.getContext("2d");
   canvasContext.clearRect(0, 0, $canvas.width, $canvas.height);
-  canvasContext.fillStyle = "white";
+  canvasContext.fillStyle = bgColor;
   canvasContext.beginPath();
   canvasContext.fillRect(0, 0, $canvas.width, $canvas.height);
   canvasContext.fill();
-  canvasContext.fillStyle = "black";
+  canvasContext.fillStyle = fgColor;
   canvasContext.font = fontWeight + " " + fontSize + "px sans-serif";
   canvasContext.textBaseline = "top";
   $canvas.style.display = "none";
   for (let lineIdx = 0; lineIdx < textLines.length; lineIdx++) {
     canvasContext.fillText(textLines[lineIdx], padding, fontSize * 1.5 * lineIdx + padding, $canvas.width);
   }
-  $("img").src = $canvas.toDataURL("image/png");
-  $("download-btn").onclick = function () {
-    let date = new Date().valueOf();
+  $$("img").src = $canvas.toDataURL("image/png");
+  $$("download-btn").onclick = function () {
+    let date = new Date().valueOf(); // Timestamp in milliseconds since 1970-01-01
     let outputImgName = "changweibo" + date + ".png";
     download($canvas, outputImgName);
   };
@@ -70,3 +72,28 @@ function download($canvas, outputImgName) {
     $downloadLinkHidden["fireEvent"]("onclick");
   }
 }
+
+function onGenerateButtonClick() {
+  textToImg("txt", "canvas", false, DEFAULT_IMG_CONFIG);
+}
+
+function onGenerateDarkButtonClick() {
+  textToImg("txt", "canvas", true, DEFAULT_IMG_CONFIG);
+}
+
+function onTextAreaKeyUp(e) {
+  console.log('eee')
+  if (!e.target.value) {
+    $$('submit-btn').disabled = true;
+    $$('submit-btn-dark').disabled = true;
+  } else {
+    $$('submit-btn').disabled = false;
+    $$('submit-btn-dark').disabled = false;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  $$("txt").onkeyup = onTextAreaKeyUp;
+  $$("submit-btn").onclick = onGenerateButtonClick;
+  $$("submit-btn-dark").onclick = onGenerateDarkButtonClick;
+});
